185e1433ae5df2c5c58b3e9b115aa73d7ac1fb5c
Dan Nguyen <dtn3423@gmail.com>
2017-11-01 01:18:42 -0400

    change quicklinks to button instead of hover

--- src/js/components/quickLink.js
+++ src/js/components/quickLink.js
@@ -1,63 +1,58 @@
 (function quickLinkFunc() {
+  function quickModel(newData) {
+    if (typeof localStorage.getItem('linkArray') !== 'string') {
+      const retrieve = // retrieve is an array of default values for the quickLinks.
+      [// Note: The default sites that I added here are dev-based, if you guys prefer anything else or you guys would want to add then tell me :)
+        {
+          title: 'Github', // A must for any developer ^^
+          url: 'https://Github.com',
+          id: 1,
+        },
+        {
+          title: 'MDN',
+          url: 'https://developer.mozilla.org/en-US/',
+          id: 2,
+        },
+        {
+          title: 'Stack Overflow', // Maybe FreeCodeCamp??
+          url: 'https://stackoverflow.com',
+          id: 3,
+        },
+      ];
 
-	function quickModel(newData) {
+      localStorage.setItem('linkArray', JSON.stringify(retrieve)); // Here, we create the "linkArray" object and we set retrieve as its value.
+    }
 
-		if(typeof localStorage.getItem("linkArray") != 'string') {
+    let retrieve = localStorage.getItem('linkArray'); // We declare the retrieve variable and assign the "linkArray" object to retrieve.
+    retrieve = JSON.parse(retrieve); // Since the objects in retrieve have been stringified we will parse them back together.
 
-			var retrieve = //retrieve is an array of default values for the quickLinks.
-			[//NOTE: The default sites that I added here are dev-based, if you guys prefer anything else or you guys would want to add then tell me :)
-			{
-				title: "Github",//A must for any developer ^^
-				url: "https://Github.com",
-				id: 1
-			},
-			{
-				title: "MDN",
-				url: "https://developer.mozilla.org/en-US/",
-				id: 2
-			},
-			{
-				title: "Stack Overflow",//Maybe FreeCodeCamp??
-				url: "https://stackoverflow.com",
-				id: 3
-			}
-			];
+    if (typeof newData === 'object') {
+      retrieve.push(newData);
+      localStorage.setItem('linkArray', JSON.stringify(retrieve));
+    }
+    return retrieve;
+  }
 
-			localStorage.setItem("linkArray", JSON.stringify(retrieve));//Here, we create the "linkArray" object and we set retrieve as its value.
+  function quickView(dataRecieve) {
+    const retrieve = dataRecieve();
+    for (let i = 0; i < retrieve.length; i += 1) { // This adds each object into the HTML page.
+      $('.quickList').append(`<li><a href="${retrieve[i].url}">${retrieve[i].title}</a></li>`);
+    }
+  }
 
-		}
+  // Fade in/out buggy
+  function toggleQuickLinks(e) {
+    const qlBox = $('.quickDropdown');
+    if (qlBox.is(':visible') && !qlBox.find(e.target).length) {
+      qlBox.hide();
+    } else if (!qlBox.is(':visible') && e.target === $('.qlToggle')[0]) {
+      qlBox.show();
+    }
+  }
 
-		var retrieve = localStorage.getItem("linkArray");//We declare the retrieve variable and assign the "linkArray" object to retrieve.
-
-		retrieve = JSON.parse(retrieve);//Since the objects in retrieve have been stringified we will parse them back together.
-
-		if (typeof newData == 'object') {
-
-			retrieve.push(newData);
-			localStorage.setItem("linkArray", JSON.stringify(retrieve));
-
-		}
-
-		return retrieve;
-
-	}
-
-	function quickView(dataRecieve) {
-
-		var retrieve = dataRecieve();
-
-		for (var i = 0; i < retrieve.length; i++) {//This adds each object into the HTML page.
-
-			$(".quickList").append("<li><a href='" + retrieve[i].url + "'>" + retrieve[i].title + "</a></li>");
-
-		}
-
-	}
-
-	window.app.quickLinkApp = {
-
-	quickModel,
-	quickView,
-
-	}
+  window.app.quickLinkApp = {
+    quickModel,
+    quickView,
+    toggleQuickLinks,
+  };
 }());
--- src/js/main.js
+++ src/js/main.js
@@ -145,27 +145,27 @@ CONTROLLER
   /* ********* QUICK LINK SECTION ********* */
 
   function quickControl() {
-    $(".addSite").click(function() {
-      $('.addUrl').fadeToggle("slow");
+    $('.addSite').click(() => {
+      $('.addUrl').fadeToggle('slow');
     });
 
-    $("#targetForm").submit(function(e) {//When the quickLinks submit button is pressed, this will grab the input values and push it to localStorage.
+    $('#targetForm').submit((e) => { // When the quickLinks submit button is pressed, this will grab the input values and push it to localStorage.
       e.preventDefault();
 
-      var titleInput = $("#titleInput").val();
-      var urlInput = $("#urlInput").val();
-      var newObject = {//In order to push this to localStorage we will want it in object form so that the for loop above can access these properties.
+      const titleInput = $('#titleInput').val();
+      const urlInput = $('#urlInput').val();
+      const newObject = { // In order to push this to localStorage we will want it in object form so that the for loop above can access these properties.
         title: titleInput,
-        url: urlInput
-      }
+        url: urlInput,
+      };
       $('.addUrl').fadeOut('slow');
-      //This resets the inputs so that it doesnt show the link you already put in.
-      $("#titleInput").val("");
-      $("#urlInput").val("");
+      // This resets the inputs so that it doesnt show the link you already put in.
+      $('#titleInput').val('');
+      $('#urlInput').val('');
 
       function updateQuickStorage() {
         quickLinkApp.quickModel(newObject);
-        $(".quickList").append("<li><a href='" + newObject.url + "'>" + newObject.title + "</a></li>");
+        $('.quickList').append(`<li><a href="${newObject.url}">${newObject.title}</a></li>`);
       }
 
       updateQuickStorage();
@@ -213,15 +213,11 @@ CONTROLLER
         }
         return response.json();
       })
-<<<<<<< HEAD
-      .then(results => cssView.successOutput(results.cssvalidation.errors, cssModel.format));
-=======
       .then(results => cssView.successOutput(results.cssvalidation.errors, cssModel.format))
       .catch((error) => {
         cssView.errorOutput();
         console.log(error);
       });
->>>>>>> 7f5736972525c5cbcaacac6770e2f3e43f045880
     $('#check-css').removeAttr('disabled', 'disabled');
   }
 
@@ -236,6 +232,25 @@ CONTROLLER
   /* ********* PAGE SPEED SECTION ********** */
 
   function loadPageSpeedChecker() {
+    function catchSpeedErrors(result) {
+      if (result.responseJSON.error) {
+        const errors = result.responseJSON.error.errors;
+        for (let i = 0, len = errors.length; i < len; i += 1) {
+          if (errors[i].reason === 'badRequest' && pagespeedModel.API_KEY === 'yourAPIKey') {
+            // console.log('Please specify your Google API key in the API_KEY variable.');
+            $('#speed-page-error').append('Please specify your Google API key in the API_KEY variable.');
+          } else {
+            // console.log(errors[i].message);
+            $('#speed-page-error').append(`${errors[i].message}`);
+          }
+        }
+        $('#loader-icon').removeClass('spin').hide();
+        $('#analyzePage').removeAttr('disabled', 'disabled');
+        $('#clearPage').removeAttr('disabled', 'disabled');
+        $('.toggle-custom-view').removeAttr('disabled', 'disabled');
+      }
+    }
+
     // Invokes the PageSpeed Insights API. The response will contain
     // JavaScript that invokes our callback with the PageSpeed results
     function runPagespeed() {
@@ -246,18 +261,12 @@ CONTROLLER
         urlStrategy = `${pagespeedModel.API_URL}&key=${pagespeedModel.API_KEY}&strategy=mobile&url=${$('#path').val()}`;
       } else { urlStrategy = `${pagespeedModel.API_URL}&key=${pagespeedModel.API_KEY}&strategy=desktop&url=${$('#path').val()}`; }
 
-      // Check to see if URL entered is valid
-      if ($('#path').get(0).checkValidity() === true) {
-        $.getJSON(urlStrategy, (result) => {
-          pagespeedView.displayPageSpeedScore(result);
+      $.getJSON(urlStrategy, (result) => {
+        pagespeedView.displayPageSpeedScore(result);
+      })
+        .fail((result) => {
+          catchSpeedErrors(result);
         });
-      } else {
-        $('#speed-page-error').append(`Sorry '${$('#path').val()}' was not valid. Please make sure to enter a valid formatted URL (https://example.com) and try again.`);
-        $('#loader-icon').removeClass('spin').hide();
-        $('#analyzePage').removeAttr('disabled', 'disabled');
-        $('#clearPage').removeAttr('disabled', 'disabled');
-        $('.toggle-custom-view').removeAttr('disabled', 'disabled');
-      }
     }
 
     // Desktop & Mobile Score trigger from URL provided
@@ -450,7 +459,8 @@ CONTROLLER
       .on('click', toggleNameInput())
       .on('click', newsfeedView.toggleNewsfeed)
       .on('click', toolboxView.toggleToolbox)
-      .on('click', colorpickerView.toggleColorPicker);
+      .on('click', colorpickerView.toggleColorPicker)
+      .on('click', quickLinkApp.toggleQuickLinks);
     $('#name-form').on('submit', setUserName);
     $('.start, .stop').on('click', togglePomodoroActive);
     $('.pause').on('click', togglePomodoroPause);
@@ -469,9 +479,9 @@ CONTROLLER
     $('.returnresults').hide();
     $('#loader-icon').hide();
     $('.color-picker-panel').hide();
+    $('.quickDropdown').hide();
     greetingView.showGreeting(greetingModel.getUserName());
     clocksView.updateTime(clocksModel.getTime());
-    setupEventListeners();
     loadSounds();
     loadNewsArticles();
     clocksHandler();
@@ -480,6 +490,7 @@ CONTROLLER
     loadBackground();
     stickyApp.stickyNoteView(stickyApp.stickyNoteModel);
     quickLinkApp.quickView(quickLinkApp.quickModel);
+    setupEventListeners();
   }
 
   window.app.controller = {
